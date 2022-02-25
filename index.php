985ff58117d0f11595955b33fb130a1bfb72aca7
darioaladuz <baladuz98@gmail.com>
2022-02-25 16:33:02 +0100

    add functionality to add todos to mysql db

--- index.php
+++ index.php
@@ -5,8 +5,56 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ToDo App</title>
+    <link rel="stylesheet" href="styles/main.css">
 </head>
 <body>
-    
+    <div id="message">
+        <?php 
+            $message = '';
+            echo $message;
+        ?>
+    </div>
+
+    <form action="/todo/index.php" method="POST">
+        <input type="text" name="task" id="task" placeholder="Add a task...">
+        <input type="submit" name="submit" id="submit" value="submit">
+    </form>
+
+    <div id="tasks">
+        <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "todo";
+            
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+              die("Connection failed: " . $conn->connect_error);
+            }
+            
+            if(isset($_POST['submit'])){
+                if(empty($_POST['task'])){
+                    $message = 'what the hell bro? add something just fucking add soemthing';
+                } else {
+                    $task = $_POST['task'];
+                    // echo $_POST['task'];
+                    $sql = "INSERT INTO tasks (task)
+                    VALUES ('$task')";
+                    
+                    if ($conn->query($sql) === TRUE) {
+                      $message = "New record created successfully";
+                    } else {
+                      $message = "Error: " . $sql . "<br>" . $conn->error;
+                    }
+                }
+            } else {
+                $message = 'There was an error';
+            }
+            
+            // $conn->close();
+        ?>
+    </div>
 </body>
 </html>
